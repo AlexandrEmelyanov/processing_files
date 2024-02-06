9857835e487a445b73f7f4e2fb04d1efd7813784
Alexander Emelyanov <emelianoff.casha@mail.ru>
2024-02-06 17:44:46 +0300

    Update admin-panel

--- files_api/admin.py
+++ files_api/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import File
 
-admin.site.register(File)
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('file', 'uploaded_at', 'processed')
+    search_fields = ('uploaded_at', 'file')
+    ordering = ('-uploaded_at',)
+    list_per_page = 10
+    list_filter = ('processed', 'uploaded_at')
